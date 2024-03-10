506204d4d1137c90ed8b5852665e7ebfbda32047
Yuvraj210103 <ys146228@gmail.com>
2024-03-10 17:17:07 +0530

    chore: deleted unused files

--- src/common/inputs/InputError.tsx
+++
@@ -1,9 +0,0 @@
-interface InputErrorProps {
-  errorMessage: string;
-}
-
-const InputError = ({ errorMessage }: InputErrorProps) => {
-  return <small className="text-textPrimaryRed  text-xs">{errorMessage}</small>;
-};
-
-export default InputError;
