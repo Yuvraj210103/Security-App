2342e7cfc6aada237dda0e50f38422f5d871ac4a
Yuvraj210103 <ys146228@gmail.com>
2024-03-15 20:02:08 +0530

    feat: create calendar view of scheduling

--- client/src/component/schedule/modal/AssignShiftModal.tsx
+++ client/src/component/schedule/modal/AssignShiftModal.tsx
@@ -91,57 +91,63 @@ const AssignShiftModal = ({
     }
   };
 
+  /*******************New***************** */
+
+  const [selectedRow, setSelectedRow] = useState(0);
+
   return (
     <Dialog
       opened={opened}
       setOpened={setOpened}
-      title="Add Employee"
-      size="350px"
+      title="Assign shift"
+      size="80%"
       isFormModal
       positiveCallback={handleSubmit}
     >
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <div className="flex flex-col">
-          <div className="flex flex-col text-sm">
-            <div className="flex items-center gap-2">
-              <span className="font-semibold ">Employee Name :</span>{" "}
-              {selectedEmp?.first_name} {selectedEmp?.last_name}
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="font-semibold">Date :</span>{" "}
-              {dayjs(selectedEmpDate).format("MMM-DD - ddd")}
-            </div>
-          </div>
-          <div className="font-semibold mt-4">Assign available shifts</div>
-          <div className="flex flex-col">
-            {availableShifts.map((data) =>
-              !data.isAssigned ? (
-                <label
-                  key={data._id}
-                  className="flex items-center gap-2 p-2 hover:bg-gray-100 cursor-pointer"
-                >
-                  <input
-                    type="checkbox"
-                    checked={formData.selectedShifts.includes(data._id)}
-                    onChange={() => handleCheckboxChange(data._id)}
-                  />
-                  <div>
-                    {data.start_time} - {data.end_time}
-                  </div>
-                </label>
-              ) : null
-            )}
-            {availableShifts.every((data) => data.isAssigned) && (
-              <p key="no-shifts">No shifts left</p>
-            )}
-          </div>
+      <div className="flex flex-col bg-gray-100 rounded-md p-4">
+        <div className="font-semibold text-lg">Assign (Unassigned shifts)</div>
+        <div className="bg-blue-500 p-1 text-xs text-surface w-fit">
+          Highlighted employee will be assigned
         </div>
-      </form>
+
+        <table className="mt-4 text-sm">
+          <thead className="">
+            <tr>
+              <th className="py-2 px-4 text-start">First Name</th>
+              <th className="py-2 px-4 text-start">Last Name</th>
+              <th className="py-2 px-4 text-start">Phone</th>
+              <th className="py-2 px-4 text-start">Week Shifts</th>
+              <th className="py-2 px-4 text-end">Week Hours</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              onClick={() => setSelectedRow(0)}
+              className={`${
+                selectedRow === 0 ? "bg-blue-500 text-surface" : "bg-surface"
+              } cursor-pointer`}
+            >
+              <td className="text-start px-4 py-2">Mark</td>
+              <td className="text-start px-4 py-2">Twain</td>
+              <td className="text-start px-4 py-2">+912222222222</td>
+              <td className="text-start px-4 py-2">1</td>
+              <td className="text-end px-4 py-2">3.0</td>
+            </tr>
+            <tr
+              onClick={() => setSelectedRow(1)}
+              className={`${
+                selectedRow === 1 ? "bg-blue-500 text-surface" : "bg-surface"
+              } cursor-pointer`}
+            >
+              <td className="text-start px-4 py-2">Jhon</td>
+              <td className="text-start px-4 py-2">Doe</td>
+              <td className="text-start px-4 py-2">+912222222222</td>
+              <td className="text-start px-4 py-2">1</td>
+              <td className="text-end px-4 py-2">3.0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </Dialog>
   );
 };
